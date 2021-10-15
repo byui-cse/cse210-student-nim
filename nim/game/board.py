1c06693c3fb17dbbf2b2e938f021409ae439c6f7
mickfish96 <mickfish1996@gmail.com>
2021-10-15 17:28:19 -0600

    added stub methods to board.py

--- nim/game/board.py
+++ nim/game/board.py
@@ -1,3 +1,13 @@
 import random
 
 # TODO: Define the Board class here
+
+class Board():
+    def __init__(self):
+        pass
+
+    def to_string(self):
+        pass
+
+    def apply(self, move):
+        pass
