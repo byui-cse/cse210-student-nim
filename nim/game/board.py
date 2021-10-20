e37f377477ffdaf3f7662408db78ffb13e51787e
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-10-20 11:40:08 -0600

    added teachers answer code to board.py  game isn't working yet

--- nim/game/board.py
+++ nim/game/board.py
@@ -8,9 +8,11 @@ class Board:
     Steretype: Information Holder
     Attributes: _piles (list): the number of piles of stones  """
         
-    def __init__(self) -> None:
+    def __init__(self):
+        """ this is a class constructor"""
         self._piles = []
-        self._prepare()
+        self._prepare()# setting up how board looks
+
 
     def apply(self, move):#public method
         
@@ -20,27 +22,40 @@ class Board:
         accepts one argument: move
         self(Board): an instance of Board  """
 
-    def is_empty():#public method
+        pile = move.get_pile()
+        stones = move.get_stones()
+        self.piles[pile] = max(0, self._piles[pile] - stones) 
+
+    def is_empty(self):#public method
         """  determins if all the stones have been removed from the board
 
         Returns: Boolean:  True if board has zero stones. False if there are stones on the board
         Args: self (Board): and instance of Board
         """
-        pass
+        no_more_piles= len(self._piles) * [0] # I dont' know why this works
+        return self._piles == no_more_piles
 
-    def to_string():#public method
+    def to_string(self):#public method
         """ converts the board data to its string representation and returns it to the caller
          Args:
           self(Board): an instance of Board
 
           Returns: string: A representation of the current board.
         """
-        pass
+        text = "\n ----------------"
+        for pile, stones in enumerate(self._piles):
+            text += (f"\n{pile}:" + "0" * stones)
+        text = "\n ----------------"
+        return text    
 
-    def _prepare(): #private method
+    def _prepare(self): #private method
         """ sets up the board with a random number of piles (2-5) containing a random number of stones (1-9) in each
 
          self(Board): an instance of Board
 
          this method sould be private. """
-        pass   
+        piles =random.randint(2,5)
+
+        for i in range(piles):
+            stones = random.randint(1,9)
+            self._piles.append(stones)
